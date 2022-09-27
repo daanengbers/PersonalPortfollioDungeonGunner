a5a329fa7345855a95e039b148257801b3304017
daanengbers <daan.engbers@gmail.com>
2022-09-27 14:49:18 +0200

    Node graph funtionality 1
    Implemented creating a node graph, adding nodes, setting node types, made node types and implemented draggin the nodes. followed till lesson 12

--- Personal Project Dungeon Gunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ Personal Project Dungeon Gunner/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Callbacks;
 using UnityEditor;
 
 public class RoomNodeGraphEditor : EditorWindow
 {
     private GUIStyle RoomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeSO currentRoomNode = null;
 
     private const float nodeWidth = 160f;
     private const float nodeHeight = 100f;
     private const int nodePadding = 25;
     private const int nodeBorder = 12;
+    private RoomNodeTypeListSO roomNodeTypeList;
 
 
    [MenuItem("Room Node Graph Editor", menuItem ="Window/Dungeon Editor/Room Node Graph Editor" )]
@@ -20,6 +24,23 @@ public class RoomNodeGraphEditor : EditorWindow
         GetWindow<RoomNodeGraphEditor>("Room Node Editor Graph");
     }
 
+
+    [OnOpenAsset(0)]
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
+    }
+
     private void OnEnable()
     {
         RoomNodeStyle = new GUIStyle();
@@ -27,17 +48,122 @@ public class RoomNodeGraphEditor : EditorWindow
         RoomNodeStyle.normal.textColor = Color.white;
         RoomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
         RoomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
     }
 
     private void OnGUI()
     {
 
-        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), RoomNodeStyle);
-        EditorGUILayout.LabelField("node 1");
-        GUILayout.EndArea();
+       if (currentRoomNodeGraph != null)
+        {
+            ProcessEvents(Event.current);
 
-        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), RoomNodeStyle);
-        EditorGUILayout.LabelField("node 1");
-        GUILayout.EndArea();
+            DrawRoomNodes();
+        }
+
+        if (GUI.changed)
+        {
+            Repaint();
+        }
+    }    
+    
+    private void ProcessEvents(Event currentEvent)
+        {
+
+            //get room node that mouse is currently over
+            if (currentRoomNode == null || currentRoomNode.isLeftClickDragging == false)
+            {
+                currentRoomNode = isMouseOverRoomNode(currentEvent);
+            }
+
+            //if mouse isnt over a node
+            if (currentRoomNode == null)
+            {
+                processRoomNodeGraphEvents(currentEvent);
+            }
+            //else process room node events
+            else
+            {
+                currentRoomNode.ProcessEvents(currentEvent);
+            }
+
+        }
+
+
+    // Check if mouse is over roomnode if yes return roomnode  
+    private RoomNodeSO isMouseOverRoomNode(Event currentEvent)
+    {
+        for(int i = currentRoomNodeGraph.roomNodeList.Count -1; i>=0; i--)
+        {
+            if(currentRoomNodeGraph.roomNodeList[i].rect.Contains(currentEvent.mousePosition))
+            {
+                return currentRoomNodeGraph.roomNodeList[i];
+            }
+        }
+        return null;
+    }
+
+    private void processRoomNodeGraphEvents(Event currentEvent)
+    {
+        switch (currentEvent.type)
+        {
+            case EventType.MouseDown:
+                processMouseDownEvent(currentEvent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void processMouseDownEvent(Event currentEvent)
+    {
+        if(currentEvent.button == 1)
+        {
+            showContextMenu(currentEvent.mousePosition);
+        }
+    }
+
+    private void showContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu menu = new GenericMenu();
+
+        menu.AddItem(new GUIContent("Create Room Node"), false, CreateRoomNode, mousePosition);
+
+        menu.ShowAsContext();
+    }
+
+    private void CreateRoomNode(object mousePositionObject)
+    {
+        CreateRoomNode(mousePositionObject, roomNodeTypeList.list.Find(x => x.IsNone));
+    }
+
+    private void CreateRoomNode(object mousePositionObject, RoomNodeTypeSO roomNodeType)
+    {
+        Vector2 mousePosition = (Vector2)mousePositionObject;
+
+        // create room node scriptable assset
+        RoomNodeSO roomNode = ScriptableObject.CreateInstance<RoomNodeSO>();
+
+        // Add room node to current room node graph room node list
+        currentRoomNodeGraph.roomNodeList.Add(roomNode);
+
+        // Set room node values
+        roomNode.Initialise(new Rect(mousePosition, new Vector2(nodeWidth, nodeHeight)), currentRoomNodeGraph, roomNodeType);
+
+        // Add room node to room node graph scriptable object asset database
+        AssetDatabase.AddObjectToAsset(roomNode, currentRoomNodeGraph);
+
+        AssetDatabase.SaveAssets();
+    }
+
+    private void DrawRoomNodes()
+    {
+        foreach (RoomNodeSO roomNode in  currentRoomNodeGraph.roomNodeList)
+        {
+            roomNode.Draw(RoomNodeStyle);
+        }
+        GUI.changed = true;
     }
 }
--- Personal Project Dungeon Gunner/Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ Personal Project Dungeon Gunner/Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class RoomNodeSO : ScriptableObject
@@ -11,4 +13,149 @@ public class RoomNodeSO : ScriptableObject
     public RoomNodeTypeSO roomNodeType;
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
 
+    #region Editor Code
+#if UNITY_EDITOR
+   
+    [HideInInspector] public Rect rect;
+    [HideInInspector] public bool isLeftClickDragging = false;
+    [HideInInspector] public bool isSelected = false;
+
+    public void Initialise(Rect rect, RoomNodeGraphSO nodeGraph, RoomNodeTypeSO roomNodeType)
+    {
+        this.rect = rect;
+        this.id = Guid.NewGuid().ToString();
+        this.name = "RoomNode";
+        this.roomNodeGraph = nodeGraph;
+        this.roomNodeType = roomNodeType;
+
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
+    }
+
+    public void Draw(GUIStyle nodeStyle)
+    {
+        // draw node box using begin area
+        GUILayout.BeginArea(rect, nodeStyle);
+
+        // start region to detect popup selection changes
+        EditorGUI.BeginChangeCheck();
+
+        // display a popup using the RoomNodeType name values that can be selected from (default to currently set roomNodeType)
+        int selected = roomNodeTypeList.list.FindIndex(x => x == roomNodeType);
+
+        int selection = EditorGUILayout.Popup("", selected, GetRoomNodeTypesToDisplay());
+
+        roomNodeType = roomNodeTypeList.list[selection];
+
+        if (EditorGUI.EndChangeCheck())
+        
+            EditorUtility.SetDirty(this);
+
+            GUILayout.EndArea();
+        
+    }
+
+    public string[] GetRoomNodeTypesToDisplay()
+    {
+        string[] roomArray = new string[roomNodeTypeList.list.Count];
+
+        for (int i = 0; i < roomNodeTypeList.list.Count; i ++)
+        {
+            if(roomNodeTypeList.list[i].DisplayInNodeGraphEditor)
+            {
+                roomArray[i] = roomNodeTypeList.list[i].roomNodeTypeName;
+            }
+        }
+        return roomArray;
+    }
+
+    public void ProcessEvents(Event currentEvent)
+    {
+        // switch cases by different eventTypes and call their functions
+        switch (currentEvent.type)
+        {
+
+            case EventType.MouseDown:
+                processMouseDownEvent(currentEvent);
+                break;
+            case EventType.MouseUp:
+                processMouseUpEvent(currentEvent);
+                break;
+            case EventType.MouseDrag:
+                processMouseDragEvent(currentEvent);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void processMouseDownEvent(Event currentEvent)
+    {
+        // if left mouse button is clicked run process
+        if(currentEvent.button == 0)
+        {
+            processLeftClickDownEvent();
+        }
+    }
+
+    private void processLeftClickDownEvent()
+    {
+        //selects the object in the unity editor when clicked
+        Selection.activeObject = this;
+
+        // toggle node selection
+        if (isSelected == true)
+        {
+            isSelected = false;
+        }
+        else
+        {
+            isSelected = true;
+        }
+
+    }
+
+    private void processMouseUpEvent(Event currentEvent)
+    {
+        // if left mouse button goes up call function
+        if (currentEvent.button == 0)
+        {
+            processLeftClickUpEvent();
+        }
+    }
+
+    private void processLeftClickUpEvent()
+    {
+        // if left mouse button goes up during dragging, set dragging to false
+        if (isLeftClickDragging)
+        {
+            isLeftClickDragging = false;
+        }
+    }
+
+    private void processMouseDragEvent(Event currentEvent)
+    {
+        // if left mouse button is dragging call function
+        if(currentEvent.button == 0)
+        {
+            processLeftMouseDragEvent(currentEvent);
+        }
+    }
+
+    private void processLeftMouseDragEvent(Event currentEvent)
+    {
+        isLeftClickDragging = true;
+
+        // currentEvent.delta measures the relative distance between the last event end the mouse position
+        DragNode(currentEvent.delta);
+        GUI.changed = true;
+    }
+
+    private void DragNode(Vector2 delta)
+    {
+        rect.position += delta;
+        EditorUtility.SetDirty(this);
+    }
+#endif
+    #endregion Editor Code
 }
